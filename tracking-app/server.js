9b78232a1139d86ba280c4fc08a10d222e17f402
Bersek3 <143874680+Bersek3@users.noreply.github.com>
2024-10-03 00:28:27 -0300

    f

--- tracking-app/server.js
+++ tracking-app/server.js
@@ -1,43 +1,32 @@
-// server.js
-const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
-const path = require('path');
-
+const express = require("express");
+const axios = require("axios");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 3000;
 
-app.use(cors()); // Habilitar CORS
 app.use(express.json());
 
-// Servir el archivo index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.post("/getTrackInfo", async (req, res) => {
+    const trackingNumbers = req.body; // Obtiene los números de seguimiento del cuerpo de la solicitud
 
-// Ruta para manejar la solicitud de seguimiento
-app.get('/track/:trackingNumber', async (req, res) => {
-    const trackingNumber = req.params.trackingNumber;
+    const options = {
+        method: 'POST',
+        url: 'https://api.17track.net/track/v2.2/gettrackinfo',
+        headers: {
+            'content-type': 'application/json',
+            '17token': '7FEB86258AED35FC8AF5D6BC3054CA0F' // Reemplaza esto con tu clave secreta
+        },
+        data: JSON.stringify(trackingNumbers) // Convierte el array de objetos a una cadena JSON
+    };
 
     try {
-        // Reemplaza 'YOUR_API_KEY' con tu clave de API de 17track
-        const apiKey = '7FEB86258AED35FC8AF5D6BC3054CA0F';
-        const url = `https://api.17track.net/v2/gettrackinfo?num=${trackingNumber}&apiKey=${apiKey}`;
-
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            return res.status(response.status).json({ error: 'Error fetching tracking information' });
-        }
-
-        const data = await response.json();
-        res.json(data); // Enviar los datos de seguimiento al cliente
+        const response = await axios.request(options);
+        res.json(response.data); // Envía la respuesta de la API al cliente
     } catch (error) {
-        console.error('Error fetching tracking information:', error);
-        res.status(500).json({ error: 'Error fetching tracking information' });
+        console.error(error);
+        res.status(500).send("Error en la solicitud a la API");
     }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
